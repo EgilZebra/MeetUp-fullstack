13971410e75eb36f392c271a3c4e04a0c254c511
Egil Ramsten <egil.ramsten@hotmail.com>
2024-10-29 23:32:38 +0100

    Made a simple error-page with return button

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,13 +5,12 @@ import {
   RouterProvider 
 } from "react-router-dom"
 import { LandingPage } from "./pages/LandingPage"
+import { ErrorPage } from "./pages/ErrorPage"
 import './App.css'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route 
-    // errorElement={<ErrorPage />}
-    >
+    <Route errorElement={<ErrorPage />} >
       <Route path="/" element={<LandingPage/>} />
     </Route>
   )
