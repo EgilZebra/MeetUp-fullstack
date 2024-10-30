0aeb1533178d28161c4834c8663d1b0029e1601e
zlatte1 <57747360+zlatte1@users.noreply.github.com>
2024-10-30 17:53:44 +0100

    x

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,30 @@
-import { 
-  Route, 
-  createBrowserRouter, 
-  createRoutesFromElements, 
-  RouterProvider 
-} from "react-router-dom"
-import { LandingPage } from "./pages/LandingPage"
-import './App.css'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage";
+import Profile from "./pages/Profile";
+import "./App.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route 
+    <Route
     // errorElement={<ErrorPage />}
     >
-      <Route path="/" element={<LandingPage/>} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/profile" element={<Profile />} />
     </Route>
-)
-)
+  )
+);
 
 function App() {
-
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
