8ddf0fef5192ac5e4fd0a2e41a512f72e1c9063b
zlatte1 <57747360+zlatte1@users.noreply.github.com>
2024-10-30 09:54:35 +0100

    selectMU: removed un. code

--- Backend/routes/meetupsRoutes.js
+++ Backend/routes/meetupsRoutes.js
@@ -4,34 +4,6 @@ const { GetCommand } = require("@aws-sdk/lib-dynamodb");
 const router = express.Router();
 require("dotenv").config();
 
-router.get("/", async (req, res) => {
-  try {
-    const result = await db.scan({
-      TableName: process.env.TABLE_NAME_MEETINGS,
-    });
-
-    const currentDate = new Date();
-    currentDate.setHours(0, 0, 0, 0);
-
-    const upcomingMeetups = result.Items.filter((meetup) => {
-      const meetupDate = new Date(meetup.date);
-      meetupDate.setHours(0, 0, 0, 0);
-      return meetupDate >= currentDate;
-    });
-
-    res.status(200).json({
-      success: true,
-      data: upcomingMeetups,
-    });
-  } catch (error) {
-    console.error("Error fetching meetups:", error);
-    res.status(500).json({
-      success: false,
-      message: "Failed to retrieve meetups",
-      error: error.message,
-    });
-  }
-});
 router.get("/:MeetingId", async (req, res) => {
   const { MeetingId } = req.params;
 
