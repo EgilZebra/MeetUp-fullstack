a806a51ef2a2b8dec657510407b0ca83c78df3ea
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-10-31 01:40:15 +0100

    revert

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,30 @@
-import { 
-  Route, 
-  createBrowserRouter, 
-  createRoutesFromElements, 
-  RouterProvider 
-} from "react-router-dom"
-import { LandingPage } from "./pages/LandingPage"
-import './App.css'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage";
+import ProfilePage from "./pages/Profile";
+import "./App.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route 
+    <Route
     // errorElement={<ErrorPage />}
     >
-      <Route path="/" element={<LandingPage/>} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Route>
-)
-)
+  )
+);
 
 function App() {
-
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,6 @@ export const LandingPage = () => {
   return (
     <div>
       <h1>Landingpage!</h1>
-      <p>sst</p>
     </div>
   );
 };
