c083911c574438034bc9523d24d268e9654a108b
Egil Ramsten <egil.ramsten@hotmail.com>
2024-11-01 20:09:54 +0100

    slight change to package.json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
    define: {
     'process.env': process.env
-  }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
