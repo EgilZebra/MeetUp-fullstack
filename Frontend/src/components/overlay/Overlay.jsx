4a54af2aaca700d5f6bf4b3a7cac00a32b38b641
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-11-02 11:51:26 +0100

    x

--- Frontend/src/components/overlay/Overlay.jsx
+++ Frontend/src/components/overlay/Overlay.jsx
@@ -5,6 +5,8 @@ const API_URL_BASE = process.env.API_URL_BASE;
 
 const Overlay = ({ isOpen, selectedMeetup, onClose, currentUserId }) => {
   if (!isOpen) return null;
+  const userName = currentUserId;
+  const meetingId = selectedMeetup.MeetingId;
 
   const registerMU = async () => {
     try {
@@ -12,12 +14,27 @@ const Overlay = ({ isOpen, selectedMeetup, onClose, currentUserId }) => {
       console.log("URL", url);
       const response = await axios.post(url, { userName, meetingId });
       console.log("response", response);
-      //const data = response.data.data.Items;
+      const data = response.data.data.Items;
       //console.log(data);
       // setMeetupsData(data);
       return data;
     } catch (error) {
       console.error("Error register meetups:", error);
+    }
+  };
+
+  const unRegisterMU = async () => {
+    try {
+      const url = API_URL_BASE + "/register";
+      console.log("URL", url);
+      const response = await axios.delete(url, {
+        data: { userName, meetingId }, // Use 'data' to send the payload with axios.delete
+      });
+      console.log("response", response);
+      const data = response.data.data.Items;
+      return data;
+    } catch (error) {
+      console.error("Error unRegister meetups:", error);
     }
   };
 
@@ -36,8 +53,12 @@ const Overlay = ({ isOpen, selectedMeetup, onClose, currentUserId }) => {
         <p>
           <strong>Participants:</strong> {selectedMeetup.participants}
         </p>
-        <button>Anmäl mig</button>
-        <button>Avanmäl mig</button>
+        <button onClick={() => registerMU(userName, meetingId)}>
+          Anmäl mig
+        </button>
+        <button onClick={() => unRegisterMU(userName, meetingId)}>
+          Avanmäl mig
+        </button>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
