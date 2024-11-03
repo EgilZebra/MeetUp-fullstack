9c82012f5224c43981ed93803400e6e80a607fc9
Egil Ramsten <145114264+EgilZebra@users.noreply.github.com>
2024-11-03 18:40:50 +0100

    Update Profile.jsx

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -10,13 +10,12 @@ import axios from "axios";
 import Overlay from "../components/overlay/Overlay";
 import CreateForm from "./CreateMeet";
 
-//const API_URL_BASE =
-// process.env.VITE_API_URL == undefined
-//    ? import.meta.env.VITE_API_URL
-//    : process.env.VITE_API_URL;
+const API_URL_BASE =
+process.env.VITE_API_URL == undefined
+   ? import.meta.env.VITE_API_URL
+    : process.env.VITE_API_URL;
 
-const API_URL_BASE = process.env.API_URL_BASE;
-const currentUserId = "test123";
+// const currentUserId = "test123";
 
 const profile = () => {
   const GoTo = useNavigate();
