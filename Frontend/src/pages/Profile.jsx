cb2ee6d902de4d98c9534efe33f45cc2536071a2
Egil Ramsten <egil.ramsten@hotmail.com>
2024-11-04 09:53:54 +0100

    updater2

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import AllMU from "../components/allMU/AllMU";
-import MyMU from "../components/myMU/MyMU";
-import SearchMU from "../components/searchMU/SearchMU";
-import CreateMU from "../components/forms/createMU/CreateMU";
-import "./style/Profile.css";
-
-const UserProfile = () => {
-  const [activeButton, setActiveButton] = useState("Min Profil");
-
-  return (
-    <div className="userProfile-wrapper">
-      <div className="userProfile-personalInfo">
-        <h1>username</h1>
-        <p>eventuell lägga till övrig personalInfo</p>
-        <button className="userProfile-personalInfo__logout">Logga ut</button>
-      </div>
-
-      <div className="userProfile-menu">
-        <button
-          className={activeButton === "Min Profil" ? "active" : ""}
-=======
 import AllMU from "../components/Allmu/AllMU";
 import MyMU from "../components/myMU/MyMU";
 import SearchMU from "../components/searchMU/SearchMU";
@@ -33,8 +10,7 @@ import axios from "axios";
 import Overlay from "../components/overlay/Overlay";
 import CreateForm from "./CreateMeet";
 
-const API_URL_BASE =
-process.env.VITE_API_URL == undefined
+const API_URL_BASE = process.env.VITE_API_URL == undefined
    ? import.meta.env.VITE_API_URL
     : process.env.VITE_API_URL;
 
@@ -111,79 +87,37 @@ const profile = () => {
       <div className="profile--menu">
         <button
           className={activeButton === "Min Profil" ? "profile--active" : ""}
->>>>>>> 1ff044f008f663b14d7743dc03cc42145d36f075
           onClick={() => setActiveButton("Min Profil")}
         >
           Min Profil
         </button>
-<<<<<<< HEAD
-
-        <button
-          className={activeButton === "Sök Meetups" ? "active" : ""}
-          onClick={() => setActiveButton("Sök Meetups")}
-=======
         <button
           className={activeButton === "Sök Meetups" ? "profile--active" : ""}
           onClick={() => {
             setActiveButton("Sök Meetups");
           }}
->>>>>>> 1ff044f008f663b14d7743dc03cc42145d36f075
         >
           Sök Meetups
         </button>
         <button
-<<<<<<< HEAD
-          className={activeButton === "Alla Meetups" ? "active" : ""}
-          onClick={() => setActiveButton("Alla Meetups")}
-=======
           className={activeButton === "Alla Meetups" ? "profile--active" : ""}
           onClick={() => {
             setActiveButton("Alla Meetups");
             // GoTo('/AllMU');
           }}
->>>>>>> 1ff044f008f663b14d7743dc03cc42145d36f075
         >
           Alla Meetups
         </button>
         <button
-<<<<<<< HEAD
-          className={activeButton === "Skapa Meetup" ? "active" : ""}
-          onClick={() => setActiveButton("Skapa Meetup")}
-=======
           className={activeButton === "Skapa Meetup" ? "profile--active" : ""}
           onClick={() => {
             setActiveButton("Skapa Meetup");
           }}
->>>>>>> 1ff044f008f663b14d7743dc03cc42145d36f075
         >
           Skapa Meetup
         </button>
       </div>
 
-<<<<<<< HEAD
-      <div className="userProfile-content">
-        {activeButton === "Min Profil" && (
-          <div className="userProfile-content__profile">
-            <MyMU />
-          </div>
-        )}
-        {activeButton === "Sök Meetups" && (
-          <div className="userProfile-content__searchMUs">
-            <SearchMU />
-          </div>
-        )}
-        {activeButton === "Alla Meetups" && (
-          <div className="userProfile-content__listMUs">
-            <AllMU />
-          </div>
-        )}
-        {activeButton === "Skapa Meetup" && (
-          <div className="userProfile-content__createMU">
-            <CreateMU />
-          </div>
-        )}
-      </div>
-=======
       <div className="profile-content">
         {activeButton === "Min Profil" && (
           <div className="profile-content__profile">
@@ -270,13 +204,9 @@ const profile = () => {
           currentUserId={currentUserId}
         />
       )}
->>>>>>> 1ff044f008f663b14d7743dc03cc42145d36f075
     </div>
   );
 };
 
-<<<<<<< HEAD
-export default UserProfile;
-=======
 export default profile;
->>>>>>> 1ff044f008f663b14d7743dc03cc42145d36f075
+
